98fd96e6c51a63112cfb8159fa807c075c0f73d7
Adir <slutzkis@gmail.com>
2024-02-22 14:13:02 +0200

    updates

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import React from 'react';
+import "./Header.css";
+
+const Header = (props) => {
+    return (
+        <div className='comp-header'>
+            <div className="logo"></div>
+        </div>
+    )
+};
+
+export default Header;
--- src/Leftbar.jsx
+++ src/Leftbar.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import React from 'react';
+import Header from './Header';
 import "./Leftbar.css";
 
 const Leftbar = (props) => {
     return (
         <div className='comp-leftbar'>
+            <Header />
             <div className='title'>History</div>
         </div>
     )

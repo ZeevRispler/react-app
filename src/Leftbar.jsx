ca382c3ee6d27f35eade08fc46d2ce9d5b89c1af
Adir <slutzkis@gmail.com>
2024-02-22 14:44:49 +0200

    updates

--- src/Leftbar.jsx
+++ src/Leftbar.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import React from 'react';
 import Header from './Header';
+import Search from './Search';
 import "./Leftbar.css";
 
 const Leftbar = (props) => {
     return (
         <div className='comp-leftbar'>
             <Header />
+            <Search />
             <div className='title'>History</div>
         </div>
     )
---
+++ src/Search.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import React from 'react';
+import "./Search.css";
+
+const Search = (props) => {
+    return (
+        <div className='comp-Search'>
+            <div className='title'>Chat</div>
+        </div>
+    )
+};
+
+export default Search;

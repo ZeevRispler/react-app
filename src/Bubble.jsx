3ca10e0d970e0501a020571a61e72c5a78f078b6
Adir <slutzkis@gmail.com>
2024-02-25 16:21:05 +0200

    updates

--- src/Bubble.jsx
+++ src/Bubble.jsx
@@ -5,7 +5,7 @@ import "./Bubble.css";
 const Bubble = (props) => {
     return (
         <div className='comp-bubble'>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sequi quis cupiditate voluptas soluta fugit iste iure. Exercitationem magnam beatae adipisci perferendis? Explicabo modi, expedita similique non tempore libero illum.</p>
+            <p>{props.content}</p>
         </div>
     )
 };
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -7,7 +7,7 @@ const Chat = (props) => {
     return (
         <div className='comp-chat'>
             <div className='title'>Chat</div>
-            <Bubble />
+            <Bubble content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sequi quis cupiditate voluptas soluta fugit iste iure. Exercitationem magnam beatae adipisci perferendis? Explicabo modi, expedita similique non tempore libero illum." />
         </div>
     )
 };

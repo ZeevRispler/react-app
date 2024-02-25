145195f25d4131d1bc400644229e79ed5c61bebb
Adir <slutzkis@gmail.com>
2024-02-25 16:13:14 +0200

    updates

---
+++ src/Bubble.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import React from 'react';
+import "./Bubble.css";
+
+const Bubble = (props) => {
+    return (
+        <div className='comp-bubble'>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sequi quis cupiditate voluptas soluta fugit iste iure. Exercitationem magnam beatae adipisci perferendis? Explicabo modi, expedita similique non tempore libero illum.</p>
+        </div>
+    )
+};
+
+export default Bubble;
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import React from 'react';
 import "./Chat.css";
+import Bubble from "./Bubble";
 
 const Chat = (props) => {
     return (
         <div className='comp-chat'>
             <div className='title'>Chat</div>
+            <Bubble />
         </div>
     )
 };

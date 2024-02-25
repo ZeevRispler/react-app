7af20a3193b7a80b518b5d7ce41c88ff12ce515b
Adir <slutzkis@gmail.com>
2024-02-25 17:34:28 +0200

    updates

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import React from 'react';
 import "./Message.css";
+import Button from "./Button"
 
 const Message = (props) => {
     return (
         <div className='comp-message'>
             <div className="input">
                 <input type='text' placeholder='Send message...' />
+                <Button label="Send" />
             </div>
         </div>
     )

2ae066826ad12c9842a047edfe8dc14e9d25957b
Adir <slutzkis@gmail.com>
2024-02-26 12:02:00 +0200

    updates

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import React from 'react';
 import "./Chat.css";
 import Bubble from "./Bubble";
+import Sources from "./Sources";
 import Message from "./Message";
 
 const Chat = (props) => {
@@ -11,7 +12,8 @@ const Chat = (props) => {
             <Bubble content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sequi quis cupiditate voluptas soluta fugit iste iure. Exercitationem magnam beatae adipisci perferendis? Explicabo modi, expedita similique non tempore libero illum." bot />
             <Bubble content="Hello, Can I get some help?" />
             <Bubble content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sequi quis cupiditate voluptas soluta fugit iste iure." bot />
-
+            <Sources content="Sources?" />
+            
             <Message />
         </div>
     )

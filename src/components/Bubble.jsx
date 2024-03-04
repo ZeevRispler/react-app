199716b2651a8eb18e529592b4da48f0be1c50c5
Adir <slutzkis@gmail.com>
2024-03-04 18:40:24 +0200

    updates

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -4,6 +4,7 @@ import "./Bubble.css";
 import IconBot from "./IconBot";
 import IconHuman from "./IconHuman";
 import Sources from "./Sources";
+import ReactMarkdown from 'react-markdown'
 
 const Bubble = (props) => {
     return (
@@ -13,7 +14,9 @@ const Bubble = (props) => {
                 : <IconHuman />
             }
             <div className="flex-column">
-                <div className='inner-bubble'>{props.content}</div>
+                <div className='inner-bubble'>
+                    <ReactMarkdown>{props.content}</ReactMarkdown>
+                </div>
                 {props.bot == 'AI' ? <div className='help-text'>Was this helpful? <button>👍</button><button>👎</button></div> : <div className='help-text'></div>}
 
                 {console.log(props.source)}

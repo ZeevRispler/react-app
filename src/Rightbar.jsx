b2644e38bba89993db758eed96833f798a4f8c0d
Adir <slutzkis@gmail.com>
2024-03-06 16:16:22 +0200

    Update Rightbar.jsx

--- src/Rightbar.jsx
+++ src/Rightbar.jsx
@@ -14,8 +14,9 @@ const Rightbar = (props) => {
             <div className='title'>Properties</div>
             <div className="inner-elements">
                 <Paragraph header="Paragraph component" content="Bibendum vehicula aenean parturient blandit aliquam. Amet ipsum turpis integer gravida pulvinar aenean dictumst faucibus." />
-                <Dropdown header="Dropdown component" content="Please select company" option={["Apple", "Samsung", "OnePlus", "Google", "Xiaomi"]} />
                 <Input header="Input component" content="Please type name" placetext="Placeholder only" />
+                <Dropdown header="Dropdown component" content="Please select company" option={["Apple", "Samsung", "OnePlus", "Google", "Xiaomi"]} />
+                <Dropdown header="Another dropdown" content="What's the best beverage?" option={["Water", "Coke", "Orange Juice", "Cider", "Coffee", "Tea", "Chai"]} />
                 <Textarea header="Textarea component" content="Please type description" placetext="Placeholder only" />
                 <Slider header="Slider component" content="Values are between 1 and 18" min="1" max="18" />
                 <Slider header="Slider component" content="Values are between 1 and 99" min="1" max="99" />

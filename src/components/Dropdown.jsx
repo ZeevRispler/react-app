2908a409a3a5bb325ca144406eaf1733887ec437
Adir <slutzkis@gmail.com>
2024-03-06 14:21:40 +0200

    updates

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import "./Dropdown.css";
 
 const Dropdown = (props) => {
+    const options = props.option;
     return (
         <div className='comp-dropdown'>
             <h3>{props.header}</h3>
             <p>{props.content}</p>
-            <input type="text" />
+            {/* <input type="text" /> */}
+            <select>
+            {options.map((option) => (
+                <option>{option}</option>
+            ))}
+            </select>
         </div>
     )
 };

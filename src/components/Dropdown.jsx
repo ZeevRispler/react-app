c6a1c34390156a348a4d3762b67357473b4fe94b
Adir <slutzkis@gmail.com>
2024-03-06 14:38:20 +0200

    Update Dropdown.jsx

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,11 +8,10 @@ const Dropdown = (props) => {
         <div className='comp-dropdown'>
             <h3>{props.header}</h3>
             <p>{props.content}</p>
-            {/* <input type="text" /> */}
             <select>
-            {options.map((option) => (
-                <option>{option}</option>
-            ))}
+                {options.map((option) => (
+                    <option>{option}</option>
+                ))}
             </select>
         </div>
     )
